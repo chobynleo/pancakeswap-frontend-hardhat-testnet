ab5d5d6cb1e357577b823b89260e853563f0ff41
chobyn <chobynleo@gmail.com>
2022-04-29 16:00:41 +0800

    remove comments

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-# 以下的运行顺序不能乱
-# 部署合约
 cd hardhat-token
 npx hardhat run scripts/deploy.js --network dev
 
@@ -9,15 +7,12 @@ npx hardhat run scripts/deploy.js --network dev
 cd ../pancake-swap-periphery
 npx hardhat run scripts/deploy.js --network dev
 
-# 由于前端需要读取tokenlists文件，这里拷贝一份挂载到public目录下，出于vue的设置，此处不能建立软链接
 cd ../pancake-frontend
 cp ./src/config/constants/tokenLists/pancake-default.tokenlist.json   ./public/
 
-# 编译sdk，放入自定义的local-pancakeswap-libs包中
 cd ../pancake-swap-sdk
 npm run build
 cp -r ./dist ./local-pancakeswap-libs/sdk/
 
-# 拷贝一份sdk到pancake-frontend工程中的libs中
 cp -r ./local-pancakeswap-libs ../pancake-frontend/node_modules
 
--- serve.sh
+++ serve.sh
@@ -1,3 +1,2 @@
-# 启动一个可以供MetaMask连接的Hardhat网络
 cd hardhat-token
 npx hardhat node
